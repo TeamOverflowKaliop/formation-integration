609e8bf2a77cc4bd8cd615fea7c42997e0d65ada
TeamOverflowKaliop <139862240+TeamOverflowKaliop@users.noreply.github.com>
2023-08-10 15:25:40 +0200

    2 files modified

--- src/components/correction/Modal/Modal.vue
+++ src/components/correction/Modal/Modal.vue
@@ -1,15 +1,19 @@
 <template>
   <Teleport to="body">
-    <div class="Modal" :class="{ 'Modal--open': modelValue }">
-      <div class="Modal__overlay"></div>
-      <div class="Modal__body">
-        <button class="Modal__close" @click="closeModal">
-          <Icon :name="iconEnum.CROSS" />
-        </button>
-        <h1 class="Modal__title">{{ title }}</h1>
-        <slot />
+    <Transition name="fade">
+      <div v-if="modelValue" class="Modal">
+        <div class="Modal__overlay"></div>
+        <div class="Modal__body">
+          <button class="Modal__close" @click="closeModal">
+            <Icon :name="iconEnum.CROSS" />
+          </button>
+          <h1 class="Modal__title">{{ title }}</h1>
+          <div class="Modal__content">
+            <slot />
+          </div>
+        </div>
       </div>
-    </div>
+    </Transition>
   </Teleport>
 </template>
 
@@ -33,4 +37,6 @@ defineProps({
 const closeModal = () => emit('update:modelValue', false);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use './Modal.scss';
+</style>
