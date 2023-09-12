075ebbfe11b5447ddf0cfbd634150cf971db5685
TeamOverflowKaliop <139862240+TeamOverflowKaliop@users.noreply.github.com>
2023-09-12 11:06:03 +0200

    remove unused Header event

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Header @header:on-menu-toggle="onMenuToggle" />
+  <Header />
   <main>
     <Homepage />
     <!--<HomepageLive />-->
@@ -12,16 +12,4 @@ import { reactive } from 'vue';
 
 import { Footer, Header } from '@/components';
 import { Homepage, HomepageLive } from '@/pages';
-
-// const state = reactive({
-//   isMenuOpen: false,
-// });
-
-// const onMenuToggle = (e) => {
-//   if (e) {
-//     document.body.classList.add('no-scroll');
-//   } else {
-//     document.body.classList.remove('no-scroll');
-//   }
-// };
 </script>
